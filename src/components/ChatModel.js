668b40206fc552dce3e442f6cad10ded3ce0aaa9
Abhimanyu Raizada <abhi.raizada21@gmail.com>
2021-07-08 22:29:47 +0530

    updated modal

--- src/components/ChatModel.js
+++ src/components/ChatModel.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useRef } from "react";
 import "./ChatModel.css";
 import "../App.css";
 
@@ -8,6 +8,7 @@ const ChatModal = ({ messages, setShowChatModal }) => {
   const textRef = useRef();
 
   const handleSend = () => {};
+  const handleEmoji = () => {};
 
   const closeOutsideChatModal = (e) => {
     if (modalRef.current === e.target) {
@@ -30,30 +31,32 @@ const ChatModal = ({ messages, setShowChatModal }) => {
 
         <div class="chat-header">
           <div>Janet Fowler</div>
-          <h6>Online Now</h6>
+          <span class="time-left">11:01</span>
         </div>
         <div class="msg-area">
           {messages.map((msg, i) =>
             i % 2 ? (
-              <div>
-                <img src=""></img>
-                <p class="msg right">
-                  <span>{msg}</span>
-                </p>
+              <div className="msg-container darker right">
+                <img src="" alt="Avatar"></img>
+                <p class="msg">{msg}</p>
+                <span class="time-right">11:00</span>
               </div>
             ) : (
-              <div>
-                <p class="msg left">
-                  <span>{msg}</span>
-                </p>
+              <div className="msg-container left">
+                <img src="" alt="Avatar"></img>
+                <p class="msg">{msg}</p>
+                <span class="time-left">11:01</span>
               </div>
             )
           )}
         </div>
         <div class="chat-footer">
-          <input type="text" ref={textRef} />
+          <button onClick={() => handleEmoji()}>
+            <i class="emoji-btn">emoji</i>
+          </button>
+          <input type="text" placeholder="Type your message" ref={textRef} />
           <button onClick={() => handleSend()}>
-            <i class="paper-plane"></i>
+            <i class="paper-plane">send</i>
           </button>
         </div>
       </div>
